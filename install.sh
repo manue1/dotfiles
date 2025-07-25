6ec02d66df7566332ea75397628e557ec0ae568d
Manuel Bergler <manue1@github.com>
2025-07-25 17:41:03 +0200

    Remove outdated brew entries and add zsh+tmux install

--- install.sh
+++ install.sh
@@ -8,3 +8,12 @@ cp -av \
     $root_dir/.tmux.conf.local \
     $root_dir/.zshrc \
     ~
+
+# oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+
+# tmux
+git clone --single-branch https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
